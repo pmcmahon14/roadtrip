196a9d9e8e27769b79c83063f697e7b79c7241a7
Patrick McMahon <pmcmahon14@gmail.com>
2019-08-11 21:18:21 -0700

    preliminary weather bar design set up

---
+++ src/Components/Weatherbar/Weatherbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Weatherbar.module.css';
+
+const Weatherbar = () => (
+    <div className={classes.bar}>
+        <p>Temp</p>
+    </div>
+);
+
+export default Weatherbar;
